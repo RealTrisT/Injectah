252e6be870de48eb92e9d6e18a9baccaba99e962
TrisT <30376152+RealTrisT@users.noreply.github.com>
2017-08-08 19:42:22 +0100

    Delete Context Menu From Registry (clean)

---
+++ UnregisterMenu.bat
@@ -0,0 +1,42 @@
+
+
+rem ---------------------------------------------------------------------------------------------------
+rem massive props to mb for this post (registry cascading menus):
+rem http://io-repo.blogspot.pt/2011/05/cascading-context-menus-via-static.html
+rem ----------
+rem massive props to Ben Gripka for this other post (admin permissions):
+rem https://stackoverflow.com/questions/1894967/how-to-request-administrator-access-inside-a-batch-file
+rem ---------------------------------------------------------------------------------------------------
+
+
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+@reg delete "HKEY_CLASSES_ROOT\dllfile\shell\Inject" /f
